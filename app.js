65fb8148bd1e552b48de57d24896546b1211042a
nodmalikov <abcmalikov@gmail.com>
2024-07-30 09:58:49 +0500

    Of site menu JS code update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const elSitenavWrapper = document.querySelector('.sitenav-wrapper');
-const elMneuToggleBtn = document.querySelector('.toggle-btn');
+const elMenuToggleBtn = document.querySelector('.toggle-btn');
 const elHeader = document.querySelector('.header');
+const elsSitenavItem = document.querySelectorAll('.sitenav-item');
 const elMainContent = document.querySelector('.main');
 
 // FUNCTION TO UPDATE MAIN CONTENT'S MARGIN-TOP BASED ON HEADER'S HEIGHT
@@ -12,13 +13,26 @@ function updateMainContentMargin() {
 // UPDATE MAIN CONTENT'S MARGIN INITIALLY
 updateMainContentMargin();
 
-if (elMneuToggleBtn) {
-    elMneuToggleBtn.addEventListener('click', function () {
+if (elMenuToggleBtn) {
+    elMenuToggleBtn.addEventListener('click', function () {
         elSitenavWrapper.classList.toggle('menu-open');
         // UPDATE MAIN CONTENT'S MARGIN AFTER TOGGLING THE MENU
         updateMainContentMargin();
     });
 }
+
+elsSitenavItem.forEach(function (elSitenavItem) {
+    elSitenavItem.addEventListener('click', function () {
+        elSitenavWrapper.classList.remove('menu-open');
+    });
+});
+
+// REMOVE MENU-OPEN CLASS WHEN CLICKING OUTSIDE THE HEADER
+document.addEventListener('click', function (event) {
+    if (!elHeader.contains(event.target)) {
+        elSitenavWrapper.classList.remove('menu-open');
+    }
+});
 
 // OPTIONAL: UPDATE MAIN CONTENT'S MARGIN IF THE HEADER'S HEIGHT CHANGES DYNAMICALLY
 new ResizeObserver(updateMainContentMargin).observe(elHeader);
